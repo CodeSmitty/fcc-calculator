7acd464cdc970a2466a7076e1c2a0668ba4e2ba2
codesmitty <varcodesmitty@gmail.com>
2020-11-07 19:56:41 -0500

    added fcc test

---
+++ src/components/calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ReactFCCtest from 'react-fcctest';
+
+const Calculator = () =>{
+
+    return(<div>
+        <div>numbers</div>
+    </div>)
+};
+
+export default Calculator;
